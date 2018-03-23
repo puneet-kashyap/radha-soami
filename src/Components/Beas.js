059fd730d7f3a8880c66541ed1d9e1942fb5c670
Puneet Kashyap <pkashyaps@gmail.com>
2018-03-23 12:30:12 -0400

    id added for GTM

--- src/Components/Beas.js
+++ src/Components/Beas.js
@@ -20,7 +20,7 @@ const Beas = () => {
                   <p><b>Radha Soami Satsang Beas</b> (RSSB) is a philosophical organization based on the spiritual teachings of all religions and dedicated to a process of inner development under the guidance of a spiritual teacher.</p>
                   <p>It was established in India in 1891 and gradually began spreading to other countries like Canada. Today RSSB holds meetings in more than 90 countries worldwide. It is a non-profit organisation with no affiliation to any political or commercial organizations.</p>
                   <p>At the core of the RSSB philosophy is a belief that there is a spiritual purpose to human life – to experience the divinity of God who resides in all of us. It is through this experience that we will realize the truth of the concept that there is only one God and we are all expressions of his love.</p>
-                  <p>Official website: <a href="http://www.rssb.org" rel="noopener noreferrer" target="_blank">Radha Soami Satsang Beas</a></p>
+                  <p>Official website: <a id="beas-official-website" href="http://www.rssb.org" rel="noopener noreferrer" target="_blank">Radha Soami Satsang Beas</a></p>
                   </div>
             </div>
 
@@ -32,21 +32,21 @@ const Beas = () => {
             </div>
 
                 <div className="row" style={{'margin':'20px 0px'}}>
-                    <div className="col-md-4">
+                    <div id="beas-toronto-address" className="col-md-4">
                         <h3>TORONTO</h3>
                         <p>Science of the Soul Study Centre </p>
                         <p>6566 Sixth Line<br/>
                         RR 1 Hornby, Ontario L0P 1E0<br/>
                         Canada</p>
                     </div>
-                    <div className="col-md-4">
+                    <div id="beas-montreal-address" className="col-md-4">
                         <h3>MONTREAL</h3>
                         <p>Garderie Minois Fripon<br/>
                         650 Rue Woodland<br/>
                         Verdun, Quebec H4H 1V8<br/>
                         Canada</p>
                     </div>
-                    <div className="col-md-4">
+                    <div id="beas-brampton-address" className="col-md-4">
                         <h3>BRAMPTON</h3>
                         <p>Central Peel Secondary School <br/>
                         Every Saturday at 06:00 PM<br/>
@@ -56,7 +56,7 @@ const Beas = () => {
                     </div>
                 </div>
                 <div className="row" style={{'margin':'20px 0px'}}>
-                  <div className="col-md-4">
+                  <div id="beas-mississauga-address" className="col-md-4">
                       <h3>MISSISSAUGA</h3>
                       <p>Weekly Satsang Centre by Public <br/>
                       Every Sunday from 10.00 AM to 11 AM<br/>
@@ -64,7 +64,7 @@ const Beas = () => {
                       3545 Morning Star Drive, Mississauga, ON L4T 1Y3<br/>
                       Canada</p>
                   </div>
-                  <div className="col-md-4">
+                  <div id="beas-kitchener-address" className="col-md-4">
                       <h3>KITCHENER</h3>
                       <p>Weekly Satsang Centre by Public <br/>
                       Every Sunday at 09:30 AM<br/>
@@ -72,7 +72,7 @@ const Beas = () => {
                       200 King St W, Kitchener, ON N2G 4G7<br/>
                       Canada</p>
                   </div>
-                  <div className="col-md-4">
+                  <div id="beas-vancouver-address" className="col-md-4">
                       <h3>VANCOUVER</h3>
                       <p>Science of the Soul Study Centre <br/>
                       2932 – 176th Street<br/>
@@ -82,7 +82,7 @@ const Beas = () => {
                 </div>
 
               <h3 style={{'marginTop':'50px'}}>Humble Request:</h3>
-              <p>If you would like to invite new people to your local Satsang ghar or centers, please share the full address and the phone number <span className="glyphicon glyphicon-phone-alt"></span> at <a href="mailto:beas@radhasoami.ca">beas@radhasoami.ca</a> so that we can publish it here and more sangat can join and benefit from Radha Soami Satsang.</p>
+              <p>If you would like to invite new people to your local Satsang ghar or centers, please share the full address and the phone number <span className="glyphicon glyphicon-phone-alt"></span> at <a id="beas-email" href="mailto:beas@radhasoami.ca">beas@radhasoami.ca</a> so that we can publish it here and more sangat can join and benefit from Radha Soami Satsang.</p>
               <p>Be the part of this noble Seva. Help to spread the word and connect with the fellow Radha Soami sangat.</p>
             </div>
         </section>
--- src/Components/Dayalbagh.js
+++ src/Components/Dayalbagh.js
@@ -30,8 +30,8 @@ const Dayalbagh = () => {
                       </p>
                       <p>Dayalbagh is the headquarters of the RadhaSoami faith, where the 8th revered leader (Sant Satguru) lives and presides over the satsang (holy service).</p>
                       <p><q><b><u>Seva</u></b> i.e. selfless service is given highest importance in Radhasoami Faith.</q></p>
-                      <p>More  information: <a href="https://en.wikipedia.org/wiki/Dayalbagh" rel="noopener noreferrer" target="_blank">Dayalbagh (Agra)</a><br/>
-                      Official Website: <a href="http://www.dayalbagh.org.in" rel="noopener noreferrer" target="_blank"> Dayalbagh</a>
+                      <p>More  information: <a id="dayalbagh-wiki" href="https://en.wikipedia.org/wiki/Dayalbagh" rel="noopener noreferrer" target="_blank">Dayalbagh (Agra)</a><br/>
+                      Official Website: <a id="dayalbagh-official-website" href="http://www.dayalbagh.org.in" rel="noopener noreferrer" target="_blank"> Dayalbagh</a>
                      </p>
                       </div>
                 </div>
@@ -42,7 +42,7 @@ const Dayalbagh = () => {
                     </div>
                 </div>
                 <div className="row" style={{'margin':'20px 0px'}}>
-                    <div className="col-md-8">
+                    <div id="dayalbagh-cambridge-address" className="col-md-8">
                         <u><h3>Cambridge</h3></u>
                         <p>Opening soon</p>
                         <p>96 Queen St E.<br/>
@@ -54,7 +54,7 @@ const Dayalbagh = () => {
                 <h3>Humble Request:</h3>
                 <p>If you would like to invite new people to your local Satsang ghar or centers, please share the full address and the phone number <span className="glyphicon glyphicon-phone-alt"></span> at <a href="mailto:dayalbagh@radhasoami.ca">dayalbagh@radhasoami.ca</a> so that we can publish it here and more sangat can join and benefit from Radha Soami Satsang.</p>
                 <p>Be the part of this noble Seva. Help to spread the word and connect with the fellow Radha Soami sangat.</p>
-                <div className="row text-center" style={{'margin':'50px 10px 10px 10px'}}>
+                <div id="dayalbagh-facebook-widget" className="row text-center" style={{'margin':'50px 10px 10px 10px'}}>
                       <div className="fb-page visible-xs"
                         data-href="https://www.facebook.com/DayalbaghCanada"
                         data-tabs="timeline,messages"
